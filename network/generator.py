41aeac9cb2ab2bcad67fc947d555ad7c91cc63ad
sid <sidd_shah95@yahoo.com>
2020-05-13 20:46:25 -0400

    Added attention optionalization

--- network/generator.py
+++ network/generator.py
@@ -12,8 +12,8 @@ def GeneratorFn(
         z_dim,
         r_scale,
         conv_size=32,
-        filters=64):
-
+        filters=64,
+        use_attn=True):
 
     # First, take the input and compress it down into a [batch_size, 100] vector, equivalent size to noise vec.
 
@@ -21,13 +21,20 @@ def GeneratorFn(
     samples //= r_scale
 
     conv2 = temporal_conv_downsample(conv1, filters, conv_size, r_scale)
-    attn2 = tf.keras.layers.Attention()([conv2, conv2])
-    attn2_norm = tf.keras.layers.BatchNormalization()(attn2)
+    if use_attn:
+        attn2 = tf.keras.layers.Attention()([conv2, conv2])
+        in_skip2 = tf.keras.layers.BatchNormalization()(attn2)
+    else:
+        in_skip2 = conv2
+
     samples //= r_scale
 
     conv3 = temporal_conv_downsample(conv2, filters, conv_size, r_scale)
-    attn3 = tf.keras.layers.Attention()([conv3, conv3])
-    attn3_norm = tf.keras.layers.BatchNormalization()(attn3)
+    if use_attn:
+        attn3 = tf.keras.layers.Attention()([conv3, conv3])
+        in_skip3 = tf.keras.layers.BatchNormalization()(attn3)
+    else:
+        in_skip3 = conv3
     samples //= r_scale
 
     conv3_flat = tf.keras.layers.Flatten()(conv3)
@@ -41,25 +48,25 @@ def GeneratorFn(
     unencoded_norm = tf.keras.layers.BatchNormalization()(unencoded)
 
     unencoded_norm_rs = tf.reshape(unencoded_norm, [-1, samples, filters], name="ReshapeUnencoded")
-    skip0 = tf.concat([unencoded_norm_rs, attn3_norm], axis=2)
+    out_skip0 = tf.concat([unencoded_norm_rs, in_skip3], axis=2)
 
     # Now do temporal deconvolutions
 
-    deconv1 = temporal_deconv(skip0, filters, r_scale, name="UpConv1")
-    skip1 = tf.keras.layers.concatenate([deconv1, attn2_norm], axis=2, name="Concat1")
+    deconv1 = temporal_deconv(out_skip0, filters, r_scale, name="UpConv1")
+    out_skip1 = tf.keras.layers.concatenate([deconv1, in_skip2], axis=2, name="Concat1")
     samples *= r_scale
 
-    deconv2 = temporal_deconv(skip1, filters, r_scale, name="UpConv2")
-    skip2 = tf.keras.layers.concatenate([deconv2, conv1], axis=2, name="Concat2")
+    deconv2 = temporal_deconv(out_skip1, filters, r_scale, name="UpConv2")
+    out_skip2 = tf.keras.layers.concatenate([deconv2, conv1], axis=2, name="Concat2")
     samples *= r_scale
     # Collapse down to a single audio channel.
-    deconv3 = temporal_deconv(skip2, 1, r_scale, name="UpConv3")
+    deconv3 = temporal_deconv(out_skip2, 1, r_scale, name="UpConv3")
     samples *= r_scale
     output = tf.reshape(deconv3, [-1, samples], name="Signal")
     output_plus_features = MelSpecFeatures(feature_size)(output)
     return output_plus_features
 
-def GeneratorModel(sample_size, feature_size, z_dim, r_scale, filter_dim):
+def GeneratorModel(sample_size, feature_size, z_dim, r_scale, filter_dim, use_attn=True):
     in1 = tf.keras.layers.Input(shape=(sample_size, feature_size + 1), name="Cond_in")
     in2 = tf.keras.layers.Input(shape=(z_dim), name="Z_in")
     value = GeneratorFn(in1, in2, sample_size, feature_size, z_dim, r_scale, filter_dim)
--- network/generator_test.py
+++ network/generator_test.py
@@ -15,6 +15,14 @@ class GeneratorTest(tf.test.TestCase):
         self.assertAllEqual(tf.shape(result), [10, 4096, 33])
         self.assertNotEqual(tf.math.count_nonzero(result), 0)
 
+    def testGeneratorOutputNoAttn(self):
+        model = generator.GeneratorModel(sample_size=4096, feature_size=32, z_dim=100, r_scale=8, filter_dim=64, use_attn=False)
+        fake_input = tf.random.uniform((10, 4096, 33))
+        fake_rand = tf.random.uniform((10, 100))
+        result = model.predict({"Cond_in": fake_input, "Z_in": fake_rand})
+        self.assertAllEqual(tf.shape(result), [10, 4096, 33])
+        self.assertNotEqual(tf.math.count_nonzero(result), 0)
+
 
 if __name__ == '__main__':
     tf.test.main()
